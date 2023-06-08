6ba6e89f087fa1b7ba9b5ffb19865fe5b179146d
Abdullah Ghazi <aghazi@lab2r4s2.42abudhabi.ae>
2023-06-08 21:58:58 +0400

    push

---
+++ Makefile
@@ -0,0 +1,34 @@
+DOCKER-COMPOSE = ./srcs/docker-compose.yml
+
+all: up
+
+set-dir:
+	@sudo mkdir -p /home/${USER}/data/wordpress
+	@sudo mkdir -p /home/${USER}/data/db
+
+
+up: set-dir
+	docker compose -f $(DOCKER-COMPOSE) up --build
+
+down:
+	docker compose -f ${DOCKER-COMPOSE} down
+
+re: clean
+	docker compose -f ${DOCKER-COMPOSE} down
+	docker compose -f ${DOCKER-COMPOSE} up -d --build
+
+clean: 	
+	docker compose -f $(DOCKER-COMPOSE) down
+	@-docker stop `docker ps -qa`
+	@-docker rm `docker ps -qa`
+	@-docker rmi -f `docker images -qa`
+	@-docker volume rm `docker volume ls -q`
+	@-docker network rm `docker network ls -q`
+	sudo rm -rf /home/${USER}/data/wordpress
+	sudo rm -rf /home/${USER}/data/db
+	@docker system prune --volumes --all --force
+	@docker rm -q $$(docker ps -qa) 2> /dev/null || true
+	@docker rmi -f $$(docker images -qa) 2> /dev/null || true
+	@docker volume rm $$(docker volume ls -q) 2> /dev/null  || true
+
+.PHONY: all re up down clean
---
+++ srcs/requirements/WordPress/tools/wp.sh
@@ -0,0 +1,42 @@
+cd /var/www/html/wordpress
+
+if [ ! -f "/var/www/html/wordpress/wp-content/wp-config.php" ]; then
+
+# Download wordpress files
+wp core download  --path="/var/www/html/wordpress" --allow-root
+
+# Generate and configure the wp-config.php file
+wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_USERPASS --dbhost=$DB_HOSTNAME --dbprefix=wp_
+
+# Runs the standard wordpress installation process - create the wordpress tables in db
+wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN --title="$WORDPRESS_DB_TITLE" --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL
+
+
+# REDIS CACHE
+wp config set WP_CACHE true --allow-root
+wp config set WP_CACHE_KEY_SALT $DOMAIN --allow-root
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set WP_REDIS_PORT 6379 --allow-root
+wp config set WP_REDIS_DATABASE 0 --allow-root
+
+wp plugin install redis-cache --activate --allow-root
+wp redis enable --allow-root
+
+# Create user in wordpress
+wp plugin update --allow-root --all
+wp user create --path="/var/www/html/wordpress" --allow-root $WORDPRESS_DB_USER $WORDPRESS_DB_USER_EMAIL --user_pass=$WORDPRESS_DB_USER_PASSWORD
+# Activate the theme
+# wp theme install $WORDPRESS_THEME --activate --allow-root
+wp --allow-root theme install "ExS Dark" 
+wp --allow-root theme activate "exs-dark"
+# Activate the plugins
+wp plugin update --allow-root --all
+
+else
+  echo "WordPress is already configured"
+fi
+
+
+# launch php-fpm
+mkdir -p /run/php/
+php-fpm7.3 -F
